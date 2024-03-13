45c926a4917cafdc09bd502df49f33bb0d7b68b2
summer bird <2718075060@qq.com>
2024-03-13 19:50:05 +0800

    初始化项目

---
+++ server/src/main/java/com/zjz/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.zjz.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
